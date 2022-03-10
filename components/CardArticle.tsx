3b9864a8715c04b0215642c66c7762473649900b
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2022-03-10 03:40:04 +0100

    implement article card UI

--- components/CardArticle.tsx
+++ components/CardArticle.tsx
@@ -5,22 +5,25 @@ import ArrowUp from './shared/ArrowUp';
 const CardArticle: React.FC<Article> = ({ article }) => {
     console.log(article.title)
     return (
-        <article className="bg-[#FBFBFB] w-full md:h-[483px]">
+        <article className="bg-[#FBFBFB] w-full pt-[70px] pb-[55px] ">
             <article className="pb-[68px] border-b-[0.5px] border-solid border-[gray3]">
-                <h2 className="text-dark mb-[49px] text-[23px] font-extrabold uppercase">{article.tech}</h2>
-                <h1 className="text-dark text-[40px] leading-[48px] font-extrabold mb-[4px]">{article.title}</h1>
-                <p className="text-gray5 text-[26px] font-normal">{article.description}</p>
+                <div className="px-[96px]">
+                    <h2 className="text-dark mb-[49px] text-[23px] font-extrabold uppercase">{article.tech}</h2>
+                    <h1 className="text-dark text-[40px] leading-[48px] font-extrabold mb-[4px]">{article.title}</h1>
+                    <p className="text-gray5 text-[26px] font-normal">{article.description}</p>
+
+                </div>
 
             </article>
 
-            <article className="flex items-center justify-between text-gray3 text-[26px]">
+            <article className="flex items-center justify-between text-gray3 text-[26px] pt-[40px] px-[96px]">
                 <div className="flex items-center">
                     <time dateTime={article.date} className="mr-[88px]">{article.date}</time>
                     <p className="mr-[88px]">{article.duration}</p>
-                    <Image src={article.image} alt={article.publisher} width={157}
-                        height={157} />
+
+                    <img src={article.image} alt={article.publisher} />
                 </div>
-                <ArrowUp strokeColour="#111111" />
+                <ArrowUp strokeColour="#111111" url={article.url} />
 
             </article>
         </article>
--- pages/article.tsx
+++ pages/article.tsx
@@ -11,14 +11,14 @@ const Article: NextPage = () => {
                 <meta name="description" content="Articles, content, post and  writings by Ejiro Asiuwhu" />
             </Head>
 
-            <header className="bg-hero-article md:h-[710px] h-[70vh] bg-fixed bg-cover bg-center flex items-center justify-center">
+            <header className="bg-hero-article md:h-[700px] w-full h-[70vh] bg-fixed bg-cover bg-top flex items-center justify-center">
                 <div className="text-center text-white ">
                     <h1 className="text-[110px] leading-none">Articles</h1>
                     <p className="uppercase">By Ejiro AsiuwHu</p>
 
                 </div>
             </header>
-            <main className="h-screen">
+            <main className="h-screen md:px-[120px] md:pt-[160px]">
                 <section>
                     {articles.map((article) => (
                         <CardArticle article={article} key={article.id} />
