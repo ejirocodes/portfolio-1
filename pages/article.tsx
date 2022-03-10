a1780b03a8f65ecd30a4ef004da2b087ea38b52b
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2022-03-10 02:08:35 +0100

    article header section

--- pages/article.tsx
+++ pages/article.tsx
@@ -9,11 +9,14 @@ const Article: NextPage = () => {
                 <meta name="description" content="Articles, content, post and  writings by Ejiro Asiuwhu" />
             </Head>
 
-            <header className="bg-hero-article h-screen bg-cover bg-center">
-                <h1>Articles</h1>
-                <p>By Ejiro AsiuwHu</p>
+            <header className="bg-hero-article md:h-[710px] h-[70vh] bg-fixed bg-cover bg-center flex items-center justify-center">
+                <div className="text-center text-white ">
+                    <h1 className="text-[110px] leading-none">Articles</h1>
+                    <p className="uppercase">By Ejiro AsiuwHu</p>
+
+                </div>
             </header>
-            <main>
+            <main className="h-screen">
 
             </main>
 
