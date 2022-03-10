35369bf8ab11c4af44618cf3884b6de969a4da6b
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2022-03-10 01:54:42 +0100

    init article page

---
+++ pages/article.tsx
@@ -0,0 +1,23 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+const Article: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Article | Ejiro Asiuwhu</title>
+                <meta name="description" content="Articles, content, post and  writings by Ejiro Asiuwhu" />
+            </Head>
+
+            <header className="bg-hero-article h-screen bg-cover bg-center">
+                <h1>Articles</h1>
+                <p>By Ejiro AsiuwHu</p>
+            </header>
+            <main>
+
+            </main>
+
+        </>
+    )
+}
+export default Article
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-article": "url('/img/ejiro-banner.png')",
+      },
+    },
   },
   plugins: [],
 };
