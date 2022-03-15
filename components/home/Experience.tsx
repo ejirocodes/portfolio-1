e47d52e358c3ad9006259e3fae32fd4ed154e08b
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2022-03-15 19:43:14 +0100

    add more experience

--- components/home/Experience.tsx
+++ components/home/Experience.tsx
@@ -15,7 +15,7 @@ export default function Experience() {
       <div>
         <div className="">
           <div className="tech-container">
-            <h2 className="tech-title text-[18px] mb-[5px] md:mb-0 ">
+            <h2 className="tech-title md:text-[30px] text-[18px] mb-[5px] md:mb-0 ">
               Arceptive{" "}
             </h2>
             <div className="flex flex-col md:w-[50%] ">
@@ -27,6 +27,101 @@ export default function Experience() {
                 aria-label="Ejiro Asiuwhu Programming Languages"
               >
                 September 2021 &#8212; Present
+              </p>
+            </div>
+          </div>
+          <LineHoriz />
+        </div>
+        <div className="">
+          <div className="tech-container">
+            <h2 className="tech-title md:text-[30px] text-[18px] mb-[5px] md:mb-0 ">
+              Spasora{" "}
+            </h2>
+            <div className="flex flex-col md:w-[50%] ">
+              <h2 className="md:text-[26px] text-[16px] mb-[10px] md:mb-0 font-semibold ">
+                Lead Frontend Engineer · Part-time
+              </h2>
+              <p
+                className="md:text-[22px] text-gray5  text-[14px] md:pr-[20px]"
+                aria-label="Ejiro Asiuwhu Programming Languages"
+              >
+                November 2020 &#8212; Present
+              </p>
+            </div>
+          </div>
+          <LineHoriz />
+        </div>
+        <div className="">
+          <div className="tech-container">
+            <h2 className="tech-title md:text-[30px] text-[18px] mb-[5px] md:mb-0 ">
+              Bizvento{" "}
+            </h2>
+            <div className="flex flex-col md:w-[50%] ">
+              <h2 className="md:text-[26px] text-[16px] mb-[10px] md:mb-0 font-semibold ">
+                Frontend / Mobile App Developer
+              </h2>
+              <p
+                className="md:text-[22px] text-gray5  text-[14px] md:pr-[20px]"
+                aria-label="Ejiro Asiuwhu Programming Languages"
+              >
+                October 2021 &#8212; Present
+              </p>
+            </div>
+          </div>
+          <LineHoriz />
+        </div>
+        <div className="">
+          <div className="tech-container">
+            <h2 className="tech-title md:text-[30px] text-[18px] mb-[5px] md:mb-0 ">
+              LogRocket{" "}
+            </h2>
+            <div className="flex flex-col md:w-[50%] ">
+              <h2 className="md:text-[26px] text-[16px] mb-[10px] md:mb-0 font-semibold ">
+                Technical Writer · Freelance
+              </h2>
+              <p
+                className="md:text-[22px] text-gray5  text-[14px] md:pr-[20px]"
+                aria-label="Ejiro Asiuwhu Programming Languages"
+              >
+                November 2020 &#8212; Present
+              </p>
+            </div>
+          </div>
+          <LineHoriz />
+        </div>
+        <div className="">
+          <div className="tech-container">
+            <h2 className="tech-title md:text-[30px] text-[18px] mb-[5px] md:mb-0 ">
+              Strapi{" "}
+            </h2>
+            <div className="flex flex-col md:w-[50%] ">
+              <h2 className="md:text-[26px] text-[16px] mb-[10px] md:mb-0 font-semibold ">
+                Technical Writer · Freelance
+              </h2>
+              <p
+                className="md:text-[22px] text-gray5  text-[14px] md:pr-[20px]"
+                aria-label="Ejiro Asiuwhu Programming Languages"
+              >
+                July 2021 &#8212; Present
+              </p>
+            </div>
+          </div>
+          <LineHoriz />
+        </div>
+        <div className="">
+          <div className="tech-container">
+            <h2 className="tech-title md:text-[30px] text-[18px] mb-[5px] md:mb-0 ">
+              Smashing Mag.{" "}
+            </h2>
+            <div className="flex flex-col md:w-[50%] ">
+              <h2 className="md:text-[26px] text-[16px] mb-[10px] md:mb-0 font-semibold ">
+                Technical Writer · Freelance
+              </h2>
+              <p
+                className="md:text-[22px] text-gray5  text-[14px] md:pr-[20px]"
+                aria-label="Ejiro Asiuwhu Programming Languages"
+              >
+                July &#8212; September 2021
               </p>
             </div>
           </div>
