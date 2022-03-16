a6e01072027a4f39f627feac9a9c475c69600f2d
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2022-03-16 03:11:50 +0100

    style work card

--- components/home/work/WorkCard.tsx
+++ components/home/work/WorkCard.tsx
@@ -2,12 +2,36 @@ import React from "react";
 
 export default function WorkCard() {
   return (
-    <section className="relative bg-[#FBFBFB]">
-      <h1 className="md:mb-[12px]">
+    <section className="relative bg-[#FBFBFB] md:w-1/2 md:px-[100px] md:py-[100px]  ">
+      <h1 className="mb-[12px] md:text-[36px] font-bold text-[24px] ">
         I build cool & amazing stuffs for the internet
       </h1>
-      <p>Open source projects, web apps, mobile app and experimentals.</p>
-      <span>
+      <p className="text-[14px] md:text-[22px] md:mb-[60px] mb-[40px] ">
+        Open source projects, web apps, mobile app and experimentals.
+      </p>
+      <div className="md:text-center text-left mt-[50px] md:mt-[120px] ">
+        <button className=" btn10 hover-main group border border-solid border-gray2 text-white bg-dark hover:text-dark hover:bg-white inline-flex items-center justify-center font-bold text-[14px] md:text-[19px] py-[28px] md:w-[305px] w-[210px] mx-auto">
+          <span className="mr-[12px]">See my work</span>
+          <span>
+            <svg
+              width="11"
+              height="13"
+              viewBox="0 0 11 13"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                className="group-hover:stroke-[#111] transition-all"
+                d="M9.26717 7.82993L7.51275 1.28236L0.965173 3.03678M7.51275 1.28236L1.48828 11.717L7.51275 1.28236Z"
+                stroke="#ffffff"
+                strokeWidth="1.44609"
+                strokeLinecap="square"
+              />
+            </svg>
+          </span>
+        </button>
+      </div>
+      <span className="absolute top-[53px] right-[58px]">
         <svg
           width="22"
           height="32"
