e6e6d5e8735adadfb59a9ae4a462c5151c7eb0af
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2022-03-20 14:44:13 +0100

    article mobile card

--- components/CardArticle.tsx
+++ components/CardArticle.tsx
@@ -5,19 +5,19 @@ import ArrowUp from "./shared/ArrowUp";
 const CardArticle: React.FC<Article> = ({ article }) => {
   return (
     <article
-      className="bg-[#FBFBFB] cursor-grab w-full md:pt-[58px] px-[30px] mb-[20px] pt-[20px] md:pb-[47px] transition min-h-inherit md:group-hover:opacity-50 md:hover:scale-[1.04]  md:hover:opacity md:mb-[30px] hover:shadow-[0_26px_49px_rgba(0,0,0,0.08)] "
+      className="bg-[#FBFBFB] cursor-grab w-full md:pt-[58px] mb-[20px] pt-[20px] md:pb-[47px] pb-[23px] transition min-h-inherit md:group-hover:opacity-50 md:hover:scale-[1.04]  md:hover:opacity md:mb-[30px] hover:shadow-[0_26px_49px_rgba(0,0,0,0.08)] "
       itemType="http://schema.org/CreativeWork"
     >
       <div className="">
-        <article className="md:pb-[50px] border-b-[0.5px] border-solid border-[gray3] ">
-          <div className="md:px-[80px] px-">
+        <article className="md:pb-[50px] pb-[30px] border-b-[0.5px] border-solid border-[gray3] ">
+          <div className="md:px-[80px] px-[30px] ">
             <h2 className="text-dark md:mb-[39px]
              mb-[19px] md:text-[20px] text-[10px] 
              md:leading-[41px] leading-[12px] font-extrabold md:tracking-[4px] tracking-[0.27em]
              uppercase group-hover:text-white">
               {article.tech}
             </h2>
-            <h1 className="text-dark md:text-[34px] text-[16px] md:leading-[34px] leading-[19px]  font-extrabold mb-[12px]">
+            <h1 className="text-dark md:text-[34px] text-[16px]  mb-[10px]  md:leading-[34px] leading-[19px]  font-extrabold md:mb-[12px]">
               {article.title}
             </h1>
             <p className="text-gray5 md:text-[22px] text-[10px] font-normal">
@@ -26,11 +26,11 @@ const CardArticle: React.FC<Article> = ({ article }) => {
           </div>
         </article>
 
-        <article className="flex items-center justify-between text-gray3 text-[26px] md:pt-[40px] pt-[17px]  md:px-[80px]  px-[30px]">
+        <article className="flex items-center justify-between text-gray3 text-[26px] md:pt-[40px]  pt-[17px]  md:px-[80px]  px-[30px] pb-">
           <div className="flex items-center text-[10px] md:text-[20px] ">
             <time
               dateTime={article.date}
-              className="mr-[70px] font-semibold "
+              className="lg:mr-[70px] md:mr-[40px] mr-[26px] font-semibold "
             >
               {article.date}
             </time>
--- components/shared/ArrowUp.tsx
+++ components/shared/ArrowUp.tsx
@@ -2,7 +2,7 @@ import React from 'react'
             // @ts-ignore
 export default function ArrowUp({ strokeColour, url }) {
     return (
-        <a href={url} target="_blank" className="cursor-pointer hover:bg-red-300  w-full h-[45px] flex items-center justify-center rounded-full" rel="noreferrer">
+        <a href={url} target="_blank" className="cursor-pointer h-full hover:bg-red-300  w-full  flex items-center justify-center rounded-full" rel="noreferrer">
             <svg viewBox="0 0 21 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M18.8887 15.7596L15.2491 2.17631L1.66576 5.81595M15.2491 2.17631L2.75098 23.8237L15.2491 2.17631Z" stroke={strokeColour} strokeWidth="2" strokeLinecap="square" />
             </svg>
